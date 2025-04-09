a09c058f80c19bf6ec2f5bc730d5e4c6a82bfc74
Gatien Genevois <gatien.gen51@gmail.com>
2025-04-09 11:21:05 +0200

    Install new bundles

--- config/bundles.php
+++ config/bundles.php
@@ -2,4 +2,6 @@
 
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
+    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+    KnpU\OAuth2ClientBundle\KnpUOAuth2ClientBundle::class => ['all' => true],
 ];
