f1cb9263196a19f0783d20d75aed6f8664bee81f
Gatien Genevois <gatien.gen51@gmail.com>
2025-04-09 11:28:38 +0200

    Create StravaUser class to create User from a Strava access token

---
+++ src/Strava/StavaUser.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Strava;
+
+use GuzzleHttp\Client;
+use GuzzleHttp\ClientInterface;
+use GuzzleHttp\Exception\GuzzleException;
+
+class StravaUser
+{
+    public function __construct(
+        private string $accessToken,
+        private ClientInterface $httpClient = new Client(),
+    ){}
+
+    public function getAccessToken(): string
+    {
+        return $this->accessToken;
+    }
+
+    public function setAccessToken(string $accessToken): void
+    {
+        $this->accessToken = $accessToken;
+    }
+
+    public function getHttpClient(): ClientInterface
+    {
+        return $this->httpClient;
+    }
+
+    public function setHttpClient(ClientInterface $httpClient): void
+    {
+        $this->httpClient = $httpClient;
+    }
+
+    /**
+     * @throws GuzzleException
+     */
+    public function getAllActivities(): array
+    {
+        $request = $this->httpClient->request('GET',
+            'https://www.strava.com/api/v3/athlete/activities',
+            ['headers' =>
+                [
+                    'Authorization' => 'Bearer ' . $this->accessToken,
+                ]
+            ]
+        )->getBody()->getContents();
+        return json_decode($request, true);
+    }
+
+
+}
