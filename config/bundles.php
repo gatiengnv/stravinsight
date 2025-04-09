1ecb7cb4de246dcbcb2f54166288ec3799edd025
Gatien Genevois <gatien.gen51@gmail.com>
2025-04-09 11:26:13 +0200

    Install bundle for twig & create HomeController with a link to strava connection page

--- config/bundles.php
+++ config/bundles.php
@@ -4,4 +4,5 @@ return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     KnpU\OAuth2ClientBundle\KnpUOAuth2ClientBundle::class => ['all' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
 ];
