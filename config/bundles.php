c0b2446649d299726fc2a41abee9fb6b402619d3
Gatien Genevois <gatien.gen51@gmail.com>
2025-05-21 14:11:02 +0200

    Add Symfony UX Turbo integration
    Integrates Symfony UX Turbo for Hotwire Turbo functionality, enabling Turbo Stream and Turbo Core features. Updates dependencies, configuration files, and assets to include support for the UX Turbo bundle.

--- config/bundles.php
+++ config/bundles.php
@@ -14,4 +14,5 @@ return [
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Zenstruck\Foundry\ZenstruckFoundryBundle::class => ['dev' => true, 'test' => true],
+    Symfony\UX\Turbo\TurboBundle::class => ['all' => true],
 ];
